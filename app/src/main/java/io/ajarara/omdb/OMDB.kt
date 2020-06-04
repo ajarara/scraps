634bdb151893e7339e7f61f554f9389629acd736
Ahmad Jarara <ajarara94@gmail.com>
2020-06-03 21:37:26 -0400

    give up on buildConfig for now, use source secrets

--- app/src/main/java/io/ajarara/omdb/OMDB.kt
+++ app/src/main/java/io/ajarara/omdb/OMDB.kt
@@ -35,7 +35,7 @@ interface OMDB {
 
 
         fun pagingTitleSearch(title: String): Flowable<SearchResponse> =
-            titleSearch(OMDB_KEY, title, 1)
+            titleSearch(Secret.OMDB_KEY, title, 1)
                 .flatMapPublisher { initialResponse ->
                     Flowable.generate(Callable { Search(initialResponse, 1) }, pagerOf(title))
                         .map { it.response }
@@ -56,7 +56,7 @@ interface OMDB {
                         search  // swallowed
                     } else {
                         val next = Search(
-                            response = titleSearch(io.ajarara.BuildConfig.OMDB_KEY, title, search.page + 1).blockingGet(),
+                            response = titleSearch(Secret.OMDB_KEY, title, search.page + 1).blockingGet(),
                             page = search.page + 1
                         )
                         emitter.onNext(next)
